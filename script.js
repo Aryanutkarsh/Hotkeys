9cb7ac9678089a1109c91ab78b182ba3d60aa6df
Aryan Utkarsh <106980972+Aryanutkarsh@users.noreply.github.com>
2023-03-22 23:56:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
-function run() {
-  fetch("https://icanhazdadjoke.com/slack")
-    .then((data) => data.json())
-    .then((jokeData) => {
-      const jokeText = jokeData.attachments[0].text;
-      const jokeElement = document.getElementById("Test");
-
-      jokeElement.innerHTML = jokeText;
-    });
-}
 var coll = document.getElementsByClassName("collapsible");
 var i;
-
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function () {
     this.classList.toggle("active");
@@ -22,7 +11,3 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
-var urli = location.hostname;
-var change = document.getElementById("Test");
-
-console.log(urli);
